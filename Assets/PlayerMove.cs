d01560425228a797d765f7f70c14c6215f4ee0b3
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-06-14 10:16:06 +0900

    EX_画面内にとどまる

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,10 +4,58 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+
+    //子オブジェクトのサイズを入れるための変数
+    private float Left, Right, Top, Bottom;
+
+    //カメラから見た画面左下の座標を入れる変数
+    Vector3 leftBottom;
+
+    //カメラから見た画面右下の座標を入れる変数
+    Vector3 RightTop;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        //カメラとプレイヤーの距離を測る(表示画面の四隅を設定するために必須)
+        var distance = Vector3.Distance(Camera.main.transform.position, transform.position);
+
+        //スクリーン画面左下の位置を設定する
+        leftBottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+
+        //スクリーン画面右下の位置を設定する
+        RightTop = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));
+
+        //子オブジェクトの数だけループ処理を行う
+        foreach (Transform child in gameObject.transform)
+        {
+            //子オブジェクトの中で一番右の位置にいたなら
+            if(child.localPosition.x >= Right)
+            {
+                //子オブジェクトのローカルX座標を右端用の変数に代入する
+                Right = child.transform.localPosition.x;
+            }
+            //子オブジェクトの中で一番左の位置にいたなら
+            if (child.localPosition.x <= Left)
+            {
+                //子オブジェクトのローカルX座標を左端用の変数に代入する
+                Left = child.transform.localPosition.x;
+            }
+            //子オブジェクトの中で一番上の位置にいたなら
+            if (child.localPosition.z >= Top)
+            {
+                //子オブジェクトのローカルZ座標を上端用の変数に代入する
+                Top = child.transform.localPosition.z;
+            }
+            //子オブジェクトの中で一番下の位置にいたなら
+            if (child.localPosition.z <= Bottom)
+            {
+                //子オブジェクトのローカルZ座標を上端用の変数に代入する
+                Bottom = child.transform.localPosition.z;
+            }
+
+        }
+
     }
 
     // Update is called once per frame
@@ -40,6 +88,9 @@ public class PlayerMove : MonoBehaviour
             //下方向に0.01動き
             pos.z -= 0.01f;
         }
-        transform.position = new Vector3(pos.x, pos.y, pos.z);
+        transform.position = new Vector3(
+            Mathf.Clamp(pos.x, leftBottom.x + transform.localScale.x - Left, RightTop.x - transform.localScale.x - Right),
+            pos.y,
+            Mathf.Clamp(pos.z, leftBottom.z + transform.localScale.z - Bottom, RightTop.z - transform.localScale.z - Top));
     }
 }
